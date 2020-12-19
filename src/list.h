ef9cda77b1f53be156f9a7c63cdae3d402f87802
Felipe Contreras <felipe.contreras@gmail.com>
2020-12-19 09:26:59 -0600

    Cleanup header defines
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- src/list.h
+++ src/list.h
@@ -1,5 +1,5 @@
-#ifndef __INTLIST_H__
-#define __INTLIST_H__
+#ifndef LIST_H
+#define LIST_H
 
 #include <stddef.h>
 
@@ -20,4 +20,4 @@ void insert_before(list *l, list_item *before, list_item *item);
 void remove_cs101(list *l, list_item *target);
 void remove_elegant(list *l, list_item *target);
 
-#endif /* !__INTLIST_H__ */
+#endif /* LIST_H */
